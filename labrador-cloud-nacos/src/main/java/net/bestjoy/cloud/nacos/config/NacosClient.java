6eaaeb3ed88ead8be7556d49d5f4f9bb83ed5b8d
SamTheBetter <414706590@qq.com>
2020-08-22 10:55:16 +0800

    +nacos client

---
+++ labrador-cloud-nacos/src/main/java/net/bestjoy/cloud/nacos/config/NacosClient.java
@@ -0,0 +1,12 @@
+package net.bestjoy.cloud.nacos.config;
+
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class NacosClient {
+
+
+}
